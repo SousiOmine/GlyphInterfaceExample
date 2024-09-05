ec64a05275b95d18d6785f4c3dc94881d43af934
Sousi Omine <110832262+SousiOmine@users.noreply.github.com>
2024-09-05 17:33:20 +0900

    押すと光る

--- FirstGlyph/app/src/main/java/com/example/firstglyph/MainActivity.java
+++ FirstGlyph/app/src/main/java/com/example/firstglyph/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.firstglyph;
 
+import android.content.ComponentName;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.nothing.ketchum.Common;
+import com.nothing.ketchum.GlyphException;
+import com.nothing.ketchum.GlyphFrame;
+import com.nothing.ketchum.GlyphManager;
+
+public class MainActivity extends AppCompatActivity{
+
+    GlyphManager gManager = null;
+
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,58 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        GlyphManager.Callback gcallback = new GlyphManager.Callback() {
+            @Override
+            public void onServiceConnected(ComponentName componentName) {
+                if (Common.is20111()) gManager.register(Common.DEVICE_20111);
+                if (Common.is22111()) gManager.register(Common.DEVICE_22111);
+                if (Common.is23111()) gManager.register(Common.DEVICE_23111);
+                try {
+                    gManager.openSession();
+                } catch(GlyphException e) {
+                    Log.e(gManager.toString(), e.getMessage());
+                }
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+                try {
+                    gManager.closeSession();
+                } catch (GlyphException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+
+
+        gManager = GlyphManager.getInstance(getApplicationContext());
+        gManager.init(gcallback);
+
+
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ///ここが光らせる処理
+                GlyphFrame.Builder builder = gManager.getGlyphFrameBuilder();
+                GlyphFrame frame = builder.buildChannelA().build();
+                gManager.toggle(frame);
+            }
+        });
     }
+
+    @Override
+    public void onDestroy()
+    {
+        try {
+            gManager.closeSession();
+        } catch (GlyphException e) {
+            throw new RuntimeException(e);
+        }
+        gManager.unInit();
+        super.onDestroy();
+    }
+
 }
